71dcd744895847e0fc69d624cd7d583b81c60fe0
Muthyala <sudheer.muthyala@hp.com>
2022-10-27 19:50:09 +0530

    mydata

--- AZ/tet.md
+++
@@ -1,2 +0,0 @@
-# classic Editor
-- Empty job
--- pD/predifix_frontend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /var/www/pc_client
-git pull
-sudo service nginx restart
-NGINX_PROC_COUNT=$(ps -ef|grep -i nginx|grep -E "master|worker"|grep -v grep |wc -l)
-PREDIFIX_BCKEND_PROC_COUNT=$(ps -ef|grep -i 4224 |grep -v grep|wc -l)
-echo "front end $NGINX_PROC_COUNT"
-echo "backend $PREDIFIX_BCKEND_PROC_COUNT"
